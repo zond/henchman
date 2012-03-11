91364f0d8cd2f93761e4c9898cb02e10c6eec3dc
Martin Bruse <martin@oort.se>
2012-03-11 09:47:58 +0100

    added lots and lots of Yard doc

--- lib/henchman.rb
+++ lib/henchman.rb
@@ -5,6 +5,9 @@ require 'yajl'
 
 require 'henchman/worker'
 
+#
+# Thin wrapper around AMQP
+#
 module Henchman
 
   extend self
@@ -12,10 +15,22 @@ module Henchman
   @@connection = nil
   @@channel = nil
 
+  #
+  # Will return a URL to the AMQP broker to use. Will get this from the <code>ENV</code> variable <code>AMQP_URL</code> if present.
+  #
+  # @return [String] a URL to an AMQP broker.
+  #
   def amqp_url
     ENV["AMQP_URL"] || "amqp://localhost/"
   end
 
+  #
+  # Will return the default options when connecting to the AMQP broker.
+  #
+  # Uses the URL from {#amqp_url} to construct these options.
+  #
+  # @return [Hash] a {::Hash} of options to AMQP.connect.
+  #
   def amqp_options
     uri = URI.parse(amqp_url)
     {
@@ -28,27 +43,38 @@ module Henchman
   rescue Object => e
     raise "invalid AMQP_URL: #{uri.inspect} (#{e})"
   end
-  
+
+  #
+  # Will return the default options to use when creating queues and exchanges.
+  #
+  # If you change the returned {::Hash} the changes will persist in this instance, so use this to configure stuff.
+  #
+  # @return [Hash] a {::Hash} of options to use when creating exchanges and queues.
+  #
   def queue_options
-    {
+    @queue_options ||= {
       :durable => true,
       :auto_delete => true
     }
   end
 
+  #
+  # Will return the default options to use when creating channels.
+  #
+  # If you change the returned {::Hash} the changes will persist in this instance, so use this to configure stuff.
+  #
+  # @return [Hash] a {::Hash} of options to use when creating channels.
+  #
   def channel_options
-    {
+    @channel_options ||= {
       :prefetch => 1,
       :auto_recovery => true
     }
   end
-
-  def subscribe_options
-    {
-      :ack => true
-    }
-  end
-
+  
+  #
+  # Will stop and deactivate {::Henchman}.
+  #
   def stop!
     with_channel do |channel|
       channel.close
@@ -60,6 +86,11 @@ module Henchman
     @@connection = nil
   end
 
+  #
+  # Will yield an open and ready connection.
+  #
+  # @param [Proc] block a {::Proc} to yield an open and ready connection to.
+  #
   def with_connection(&block)
     @@connection = AMQP.connect(amqp_options) if @@connection.nil? || @@connection.status == :closed
     @@connection.on_open do 
@@ -67,6 +98,11 @@ module Henchman
     end
   end
 
+  #
+  # Will yield an open and ready channel.
+  #
+  # @param [Proc] block a {::Proc} to yield an open and ready channel to.
+  #
   def with_channel(&block)
     with_connection do |connection|
       @@channel = AMQP::Channel.new(connection, channel_options) if @@channel.nil? || @@channel.status == :closed
@@ -76,12 +112,22 @@ module Henchman
     end
   end
 
+  #
+  # Will yield an open and ready direct exchange.
+  #
+  # @param [Proc] block a {::Proc} to yield an open and ready direct exchange to.
+  #
   def with_direct_exchange(&block)
     with_channel do |channel|
       channel.direct(AMQ::Protocol::EMPTY_STRING, queue_options, &block)
     end
   end
 
+  #
+  # Will yield an open and ready queue.
+  #
+  # @param [Proc] block a {::Proc} to yield an open and ready queue to.
+  #
   def with_queue(queue_name, &block)
     with_channel do |channel|
       channel.queue(queue_name, queue_options) do |queue|
@@ -90,10 +136,23 @@ module Henchman
     end
   end
 
+  #
+  # Publish a message synchronously.
+  #
+  # @param [String] queue_name the name of the queue to publish to.
+  # @param [Object] message the message to publish
+  #
   def publish(queue_name, message)
     EM::Synchrony.sync(apublish(queue_name, message))
   end
 
+  #
+  # Publish a message asynchronously.
+  #
+  # @param (see #publish)
+  #
+  # @return [EM::Deferrable] a deferrable that will succeed when the publishing is done.
+  #
   def apublish(queue_name, message)
     deferrable = EM::DefaultDeferrable.new
     with_direct_exchange do |exchange|
@@ -103,7 +162,18 @@ module Henchman
     end
     deferrable
   end
-
+  
+  #
+  # Consume messages synchronously.
+  #
+  # @param [String] queue_name the name of the queue to consume messages from.
+  # @param [Proc] block the block to call for each message. The block will be 
+  #   <code>instance_eval</code>ed inside a {::Henchman::Worker::Task} and have 
+  #   access to all the particulars of the message through that instance.
+  #
+  # @return [Henchman::Worker] a {::Henchman::Worker} that will execute {::Henchman::Worker::Tasks} for
+  #   each received message.
+  #
   def consume(queue_name, &block) 
     worker = Worker.new(queue_name, &block)
     with_queue(queue_name) do |queue|
--- lib/henchman/worker.rb
+++ lib/henchman/worker.rb
@@ -1,24 +1,66 @@
 
 module Henchman
 
+  #
+  # A class that handles incoming messages.
+  #
   class Worker
+
+    #
+    # The handling of an incoming message.
+    #
     class Task
+      #
+      # [AMQP::Header] The metadata of the message.
+      #
       attr_accessor :headers
+      #
+      # [Object] The message itself
       attr_accessor :message
+      #
+      # [Henchman::Worker] the {::Henchman::Worker} this {::Henchman::Worker::Task} belongs to.
+      #
       attr_accessor :worker
+      #
+      # [Exception] any {::Exception} this {::Henchman::Worker::Task} has fallen victim to.
+      #
       attr_accessor :exception
+      #
+      # [Object] the result of executing this {::Henchman::Worker::Task}.
+      #
       attr_accessor :result
+      #
+      # Create a {::Henchman::Worker::Task} for a given {::Henchman::Worker}.
+      #
+      # @param [Henchman::Worker] worker the {::Henchman::Worker} creating this {::Henchman::Worker::Task}.
+      #
       def initialize(worker)
         @worker = worker
       end
+      #
+      # Handle an exception for a {::Henchman::Worker::Task}.
+      #
+      # @param [Exception] exception the {::Exception} that happened.
+      #
       def handle_error(exception)
         @exception = exception
         @result = instance_eval(&(worker.error_handler))
       end
+      #
+      # Call this {::Henchman::Worker::Task}.
+      #
       def call
         @result = instance_eval(&(worker.block))
         forward
       end
+      #
+      # Potentially forward the results of this {::Henchman::Worker::Task} to another queue.
+      #
+      # If the result of the {::Henchman::Worker::Task} is a {::Hash} that includes
+      # <code>"next_queue"</code> the message to this {::Henchman::Worker::Task} will be merged
+      # with the result of this {::Henchman::Worker::Task} and sent to the value of the 
+      # <code>"next_queue"</code> key.
+      #
       def forward
         if result.is_a?(Hash) && message.is_a?(Hash)
           queue_name = result.delete("next_queue")
@@ -27,17 +69,39 @@ module Henchman
           end.resume
         end
       end
+      #
+      # Acknowledge this message.
+      #
       def ack!
         headers.ack
       end
+      #
+      # Unsubscribe the {::Henchman::Worker} of this {::Henchman::Worker::Task} from the queue it subscribes to.
+      # 
       def unsubscribe!
         worker.unsubscribe!
       end
     end
+    #
+    # [String] the name of the queue this {::Henchman::Worker} listens to.
+    #
     attr_accessor :queue_name
+    #
+    # [Proc] the {::Proc} handling errors for this {::Henchman::Worker}.
+    #
     attr_accessor :error_handler
+    #
+    # [AMQP::Consumer] the consumer feeding this {::Henchman::Worker} with messages.
+    #
     attr_accessor :consumer
+    #
+    # [Proc] the {::Proc} handling the messages for this {::Henchman::Worker}.
+    #
     attr_accessor :block
+    #
+    # @param [String] queue_name the name of the queue this worker listens to.
+    # @param [Proc] block the {::Proc} that will handle the messages for this {::Henchman::Worker}.
+    #
     def initialize(queue_name, &block)
       @block = block
       @queue_name = queue_name
@@ -46,12 +110,26 @@ module Henchman
         STDERR.puts(exception.backtrace.join("\n"))
       end
     end
+    #
+    # @return [Henchman::Worker::Task] a {::Henchman::Worker::Task} for this {::Henchman::Worker}.
+    #
     def task
       Task.new(self)
     end
+    #
+    # Give this {::Henchman::Worker} a custom error handler.
+    #
+    # @param [Proc] block the {::Proc} to use for errors in this {::Henchman::Worker}. 
+    #   The {::Proc} will be <code>instance_eval</code>ed inside a {::Henchman::Worker::Task}
+    #   and have access to all the particulars of the message causing the {::Exception} through
+    #   that instance.
+    #
     def error(&block)
       @error_handler = block
     end
+    #
+    # Unsubscribe this {::Henchman::Worker} from its queue.
+    #
     def unsubscribe!
       deferrable = EM::DefaultDeferrable.new
       consumer.cancel do
